a725966498aa0acdded4cb5f1766f0bd28f49417
Diego Almeida <diego.almeida@outlook.com>
2023-01-20 15:08:08 -0300

    added env variables

---
+++ src/lib/endpoints.ts
@@ -0,0 +1,6 @@
+import * as dotenv from "dotenv";
+
+export default {
+  fileURL: process.env.FILE_URL,
+  coverURL: process.env.COVER_URL,
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "./lib/prisma";
+import endpoints from "./lib/endpoints";
 
 export async function appRoutes(app: FastifyInstance) {
   app.get("/magazines", async () => {
     const magazines = await prisma.magazine.findMany();
-    return magazines;
+
+    return {
+      fileUrl: endpoints.fileURL,
+      coverUrl: endpoints.coverURL,
+      data: magazines,
+    };
   });
 }
